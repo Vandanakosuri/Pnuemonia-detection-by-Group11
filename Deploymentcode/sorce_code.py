857a49ea062315427ee58417c2377ef64677bd4a
Vandanakosuri <145925955+Vandanakosuri@users.noreply.github.com>
2023-10-01 13:30:01 -0500

    Add files via upload

---
+++ Deploymentcode/sorce_code.py
@@ -0,0 +1,12 @@
+import pickle
+
+print("java")
+
+
+
+'''
+#Saving the model to disk
+pickle.dump(regressor, open("model.pkl"),'wb')
+
+model=pickle.load(open('model.pk1'), 'rb')
+print(model.predict([[2, 9, 6]]))'''
